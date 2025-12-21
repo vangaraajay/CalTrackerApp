fc10aedb72e8ce5f85db62e7110a5a6f7de9632b
Ajay Vangara <vangaraajay@gmail.com>
2025-12-21 16:05:42 -0500

    Tried to deploy with docker... works but might move to langchain over bedrock agents for simplicity

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,15 @@ rm -rf ./lambda-functions/tools/build_lambda
 mkdir -p ./lambda-functions/tools/build_lambda
 cp ./lambda-functions/tools/meal_tools.py ./lambda-functions/tools/build_lambda/
 
-pip3 install --target ./lambda-functions/tools/build_lambda/ -r ./lambda-functions/tools/requirements.txt
+# Build dependencies inside Amazon Linux 2 Docker
+docker run --rm -v $(pwd):/var/task --platform linux/amd64 --entrypoint bash public.ecr.aws/lambda/python:3.11 -c "
+    python3 -m pip install --upgrade pip && \
+    python3 -m pip install --target /var/task/lambda-functions/tools/build_lambda -r /var/task/lambda-functions/tools/requirements.txt
+"
+#docker run --rm -v $(pwd):/var/task public.ecr.aws/lambda/python:3.11 bash -c "
+#    yum install -y python3 python3-devel python3-pip zip gcc && \
+#    pip3 install --target /var/task/lambda-functions/tools/build_lambda -r /var/task/lambda-functions/tools/requirements.txt
+#"
 
 
 cd ./lambda-functions/tools/build_lambda
